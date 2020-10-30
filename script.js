6d2601f6a81c64501907283c9404ace2412f13f3
Preeti Mammen <62972873+preeti-mammen@users.noreply.github.com>
2020-10-30 13:21:46 +1100

    Update script.js
    updated script

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let filmData = document.getElementById("filmData")
 let peopleData = document.getElementById("peopleData");
-let divPlanets = document.getElementById("planetsData");
-let divSpecies = document.getElementById("speciesData");
-let divStarShips = document.getElementById("starshipsData");
-let divVehicles = document.getElementById("vehiclesData");
+let planetsData = document.getElementById("planetsData");
+let speciesData = document.getElementById("speciesData");
+let starshipsData = document.getElementById("starshipsData");
+let vehiclesData = document.getElementById("vehiclesData");
 
 const urls = [
 	'https://swapi.dev/api/films/',
@@ -24,14 +24,13 @@ const getAllData = async function (allUrls) {
         return data;
       }),
     );
-    console.log(films.value.results);
-    console.log(peoples.value.results);
-    console.log(planets.value.results);
-    console.log(species.value.results);
-    console.log(starships.value.results);
-    console.log(vehicles.value.results);
+   
     showfilmdata(films.value.results);
     showpeopledata(peoples.value.results);
+    showplanetsdata(planets.value.results);
+    showspeciesdata(species.value.results);
+    showstarshipsdata(starships.value.results);
+    showvehiclesdata(vehicles.value.results);
   } catch (err) {
     console.log("Error Encountered !! ", err);
   }
@@ -77,6 +76,86 @@ function showpeopledata(data) {
   // Setting innerHTML as tab variable 
   peopleData.innerHTML = tab; 
 } 
+
+function showplanetsdata(data) { 
+  let tab =  
+      `<tr> 
+        <th>Name</th> 
+        <th>Climate</th> 
+        <th>Terrain</th> 
+       </tr>`; 
+  
+  // Loop to access all rows  
+  for (let r of data) { 
+      tab += `<tr>  
+  <td>${r.name} </td> 
+  <td>${r.climate}</td> 
+  <td>${r.terrain}</td>     
+</tr>`; 
+  } 
+  // Setting innerHTML as tab variable 
+  planetsData.innerHTML = tab; 
+} 
+
+function showspeciesdata(data) { 
+  let tab =  
+      `<tr> 
+        <th>Name</th> 
+        <th>Language</th> 
+        <th>Classification</th> 
+       </tr>`; 
+  
+  // Loop to access all rows  
+  for (let r of data) { 
+      tab += `<tr>  
+  <td>${r.name} </td> 
+  <td>${r.language}</td> 
+  <td>${r.classification}</td>     
+</tr>`; 
+  } 
+  // Setting innerHTML as tab variable 
+  speciesData.innerHTML = tab; 
+} 
+
+function showstarshipsdata(data) { 
+  let tab =  
+      `<tr> 
+        <th>Name</th> 
+        <th>Model</th> 
+        <th>Manufacturer</th> 
+       </tr>`; 
+  
+  // Loop to access all rows  
+  for (let r of data) { 
+      tab += `<tr>  
+  <td>${r.name} </td> 
+  <td>${r.model}</td> 
+  <td>${r.manufacturer}</td>     
+</tr>`; 
+  } 
+  // Setting innerHTML as tab variable 
+  starshipsData.innerHTML = tab; 
+}
+
+function showvehiclesdata(data) { 
+  let tab =  
+      `<tr> 
+        <th>Name</th> 
+        <th>Model</th> 
+        <th>Manufacturer</th> 
+       </tr>`; 
+  
+  // Loop to access all rows  
+  for (let r of data) { 
+      tab += `<tr>  
+  <td>${r.name} </td> 
+  <td>${r.model}</td> 
+  <td>${r.manufacturer}</td>     
+</tr>`; 
+  } 
+  // Setting innerHTML as tab variable 
+  vehiclesData.innerHTML = tab; 
+}
 
 getAllData(urls);
 
